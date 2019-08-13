b4b7f0501884c9fcd79c1113fce1d61c309b37ad
Ronison Rodrigues <ronisonrodrigues@outlook.com>
2019-08-13 17:33:03 -0400

    enviando mensagem para microserviço conectado

--- server/server.js
+++ server/server.js
@@ -3,10 +3,18 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 io.on('connection', socket => {
+
     const { microservice } = socket.handshake.query;
-    console.log(microservice + 'conectado');
+    console.log(microservice + ' conectado');
+    
+    io.emit('server', {
+        'message': 'Conectado com sucesso'
+    });
+
 });
 
 http.listen(3333, function(){
+
   console.log('listening on *:3333');
+
 });
