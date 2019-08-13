a1998506989075c7876dd4e4ae082e0ca79d409e
Ronison Rodrigues <ronisonrodrigues@outlook.com>
2019-08-13 17:32:30 -0400

    Recebendo mensagem do servidor

--- microservice/microservice.js
+++ microservice/microservice.js
@@ -3,3 +3,7 @@ const socket = io('http://localhost:3333', {
     query: { microservice: 'microservice-01' }
 });
 
+socket.on('server', data => {
+    const { message } = data;
+    console.log('Servidor disse: ' + message);
+});
